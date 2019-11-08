6451f67e2a27b1a7170c801f35920dedf9e3f0a2
Andreas Pandzic <andreas.pandzic@hotmail.com>
2019-11-08 12:26:07 +0100

    changeImage function update
    click 'on' image changes image

--- changeImage.js
+++ changeImage.js
@@ -1,9 +1,23 @@
-function changeImage() {
-    let image = document.getElementById('myImage');
-        if (image.src.match("/images/red-car.jpg")) {
-            image.src = "/images/another-car.jpg";
-        }
-        else{
-            image.src = "/images/motorcycle.jpg";
-        }
+// function changeImage() {
+//     let image = document.getElementById('myImage');
+//         if (image.src.match("/images/red-car.jpg")) {
+//             image.src = "/images/another-car.jpg";
+//         }
+//         else{
+//             image.src = "/images/motorcycle.jpg";
+//         }
+// }
+
+
+let imageTracker = "r";
+function changeImg() {
+    let image = document.getElementById('myImg');
+    if (imageTracker == "r"){
+        image.src = "/images/another-car.jpg";
+        imageTracker = "a";
+    }else {
+        image.src = "/images/red-car.jpg";
+        imageTracker = "r";
+    }
 }
+
